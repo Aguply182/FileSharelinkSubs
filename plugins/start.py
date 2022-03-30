0a68f1a59de18ec5973c557e6725cf0948e43397
Aguply182 <101477884+Aguply182@users.noreply.github.com>
2022-03-30 21:58:36 +0800

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -217,7 +217,7 @@ async def ping_pong(client, m: Message):
     m_reply = await m.reply_text("Pinging...")
     delta_ping = time() - start
     await m_reply.edit_text(
-        "<b>PONG!!</b>🏓 \n"
+        "<b>PING!!</b>✨ \n"
         f"<b>• Pinger -</b> <code>{delta_ping * 1000:.3f}ms</code>\n"
         f"<b>• Uptime -</b> <code>{uptime}</code>\n"
     )
